d04b213d693bfbc7799e813b7e07a4ca490d8831
jihyun-j <jihyun9142@gmail.com>
2023-11-23 12:36:18 +0900

    redux todo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import todos from "../redux/modules/todos";
+import { addTodo } from "../redux/modules/todos";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div></div>;
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const [isDone, setIsDone] = useState(false);
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeContents = (e) => {
+    setContents(e.target.value);
+  };
+
+  const todos = useSelector((state) => state.todos);
+
+  const dispatch = useDispatch();
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id,
+      title,
+      contents,
+      isDone,
+    };
+
+    dispatch(addTodo(newTodo));
+  };
+  return (
+    <div>
+      <TodoForm>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          value={contents}
+          onChange={onChangeContents}
+        />
+        <input type="submit" onSubmit={addTodoHandler} />
+      </TodoForm>
+
+      {todos.map((todo) => {
+        return (
+          <ul key={todo.id}>
+            <li>{todo.id}</li>
+            <li>{todo.title}</li>
+            <li>{todo.isDone}</li>
+            <li>{todo.contents}</li>
+            <button>삭제</button>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
+const TodoForm = styled.form`
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+`;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -10,14 +10,32 @@ const initialState = [
   },
 ];
 
+const ADD_TODO = "todos/ADD_TODO";
+const DELETE_TODO = "todos/DELETE_TODO";
+const SWITCH_TODO = "todos/SWITCH_TODO";
+
+export const addTodo = (payload) => {
+  return { type: ADD_TODO, payload };
+};
+
+export const deleteTodo = (payload) => {
+  return { type: DELETE_TODO, payload };
+};
+
+export const switchTodo = (payload) => {
+  return { type: SWITCH_TODO, payload };
+};
+
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return; //TODO: 여기 작성
+      // const newTodo = action.payload;
+      return { ...state, id: action.payload.id };
 
     case "DELETE_TODO":
-      return; //TODO: 여기 작성
+      const todoId = action.payload;
+      return state.filter((todo) => todo.id !== todoId);
 
     case "SWITCH_TODO":
       return; //TODO: 여기 작성
